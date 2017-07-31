bc755f49d1043e33aee41f207bc8745d49689631
julien_stien <julien.stien@omendo.com>
2017-07-31 13:07:35 +0200

    security

--- index.php
+++ index.php
@@ -5,36 +5,44 @@
 	 * Date: 31/07/2017
 	 * Time: 11:44
 	 */
-	ini_set('display_errors', 1);
-	ini_set('error_reporting', E_ALL);
+	if(isset($_GET['t']) && $_GET['t'] == 'hwlkfdjhwskcvlnw_ksjhdqizsudkgserhytr5675274'){
+		ini_set('display_errors', 1);
+		ini_set('error_reporting', E_ALL);
 
-	function getDirContents($dir, $filter = '', &$results = array()) {
-		$files = scandir($dir);
+		function getDirContents($dir, $filter = '', &$results = array())
+		{
+			$files = scandir($dir);
 
-		foreach($files as $key => $value){
-			$path = realpath($dir.DIRECTORY_SEPARATOR.$value);
+			foreach($files as $key => $value){
+				$path = realpath($dir . DIRECTORY_SEPARATOR . $value);
 
-			if(!is_dir($path)) {
-				if(empty($filter) || preg_match($filter, $path)) $results[] = $path;
-			} elseif($value != "." && $value != "..") {
-				getDirContents($path, $filter, $results);
+				if(!is_dir($path)){
+					if(empty($filter) || preg_match($filter, $path))
+						$results[] = $path;
+				}elseif($value != "." && $value != ".."){
+					getDirContents($path, $filter, $results);
+				}
 			}
+
+			return $results;
 		}
 
-		return $results;
+		$relativePathGetted = isset($_GET[ 'scorm_seq_file' ]) ? '../courses/' . substr($_GET[ 'scorm_seq_file' ],
+		                                                                                strlen(getcwd()) + 1) : '';
+
+		$allHtmlFiles = getDirContents('../courses', '/\.html$/');
+		//$allHtmlFiles = array_merge($allHtmlFiles, getDirContents('SPIAC2017', '/\.html$/'));
+		$scormList = "";
+		foreach($allHtmlFiles as $val)
+			$scormList .= '<option value="' . $val . '" ' . ((isset($_GET[ 'scorm_seq_file' ]) && $val == $_GET[ 'scorm_seq_file' ]) ? 'selected' : '') . '>' . $val . '</option>';
+
+		$indexTpl = file_get_contents('index.html');
+		$output = str_replace('{{scorm_list}}', $scormList, $indexTpl);
+		$output = str_replace('{{scorm_seq_file}}', $relativePathGetted, $output);
+		$output = str_replace('{{t}}', 'hwlkfdjhwskcvlnw_ksjhdqizsudkgserhytr5675274', $output);
+
+		echo $output;
 	}
-
-	$relativePathGetted = isset($_GET['scorm_seq_file']) ? '../courses/'.substr($_GET['scorm_seq_file'], strlen(getcwd())+1) : '';
-
-	$allHtmlFiles = getDirContents('../courses', '/\.html$/');
-	//$allHtmlFiles = array_merge($allHtmlFiles, getDirContents('SPIAC2017', '/\.html$/'));
-	$scormList = "";
-	foreach($allHtmlFiles as $val)
-		$scormList .= '<option value="'.$val.'" '.((isset($_GET['scorm_seq_file']) && $val==$_GET['scorm_seq_file']) ? 'selected' : '').'>'.$val.'</option>';
-
-	$indexTpl = file_get_contents('index.html');
-	$output = str_replace('{{scorm_list}}', $scormList, $indexTpl);
-	$output = str_replace('{{scorm_seq_file}}', $relativePathGetted, $output);
-
-	echo $output;
+	else
+		die('ACCESS ERROR = NOT ALLOWED');
 	?>
